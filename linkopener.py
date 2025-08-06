165b9a3a99443cfdc3a038d419876062c19e9e5b
helloyashsharma <167448808+helloyashsharma@users.noreply.github.com>
2025-08-06 16:53:54 +0530

    Update linkopener.py
    1. Added check for content type

--- linkopener.py
+++ linkopener.py
@@ -3,6 +3,28 @@ import urllib.parse
 import pyperclip
 import time
 
+def openlink(line):
+
+    # find position of last space (' ')
+    last_space_index = line.rstrip().rfind(' ')
+
+    # remove last word and space
+    if last_space_index != -1:
+        trimmed_line = line[:last_space_index]
+    else:
+        print(f"Line without last word")
+
+    # rewrite line in url format
+    encoded_line = urllib.parse.quote(trimmed_line)
+    final_line = f'https://music.apple.com/in/search?term={encoded_line}'
+
+    pyautogui.hotkey('ctrl', 't')
+    pyperclip.copy(final_line)
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.press('enter')
+
+    pyperclip.copy(line)
+
 def lineparser():
 
     with open('formatted_tracks.txt', 'r', encoding='utf-8') as file:
@@ -11,27 +33,24 @@ def lineparser():
     for line in reversed(lines):
         line = line.rstrip() # remove trailing whitespaces
 
-        # find position of last space (' ')
-        last_space_index = line.rstrip().rfind(' ')
+        # split into words
+        words = line.split()
 
-        # remove last word and space
-        if last_space_index != -1:
-            trimmed_line = line[:last_space_index]
+        last_word = words[-1]
+
+        if last_word == "Explicit":
+            openlink(line)
+
+            # for explicit music, jump to url bar and type explicit
+            pyautogui.hotkey('ctrl', 'l')
+            pyautogui.typewrite('explicit')
+            pyautogui.press('esc')
+        
         else:
-            print(f"Line without last word")
-
-        # rewrite line in url format
-        encoded_line = urllib.parse.quote(trimmed_line)
-        final_line = f'https://music.apple.com/in/search?term={encoded_line}'
-
-        pyautogui.hotkey('ctrl', 't')
-        pyperclip.copy(final_line)
-        pyautogui.hotkey('ctrl', 'v')
-        pyautogui.press('enter')
-
-        pyperclip.copy(line)
+            openlink(line)
 
         time.sleep(5)
+        pyautogui.hotkey('ctrl', 'w')
 
 
 # initiate
