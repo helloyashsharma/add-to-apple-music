1a4eb813dd9d3d5fbe04de5e37169881afe35d98
helloyashsharma <167448808+helloyashsharma@users.noreply.github.com>
2025-08-10 03:02:21 +0530

    automatic scaling and viewport
    1. Automatically setting scaling factor and viewport size

--- login.py
+++ login.py
@@ -9,12 +9,12 @@ with sync_playwright() as p:
         user_data_dir=profile_path, 
         channel='chrome', 
         headless=False, 
-        viewport={"width": 1280, "height": 593}, 
-        device_scale_factor=1.5, 
+        no_viewport=True, 
         user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36', 
         locale='en-IN', 
         timezone_id='Asia/Kolkata', 
         args=[
+            '--start-maximized'
             '--disable-blink-features=AutomationControlled',
             '--no-sandbox',
             '--disable-dev-shm-usage',
