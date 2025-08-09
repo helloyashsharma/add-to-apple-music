7d42c30cfd65885f60a621baa95f12c724be5e8e
helloyashsharma <167448808+helloyashsharma@users.noreply.github.com>
2025-08-10 04:13:23 +0530

    updated stealth
    1. Using 'tf-playwright-stealth' instead of js and other flags for stealth

--- login.py
+++ login.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import sync_playwright
+from playwright_stealth import stealth_sync
 import keyboard
 import os
 
@@ -13,34 +14,14 @@ with sync_playwright() as p:
         user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36', 
         locale='en-IN', 
         timezone_id='Asia/Kolkata', 
-        ignore_default_args=['--enable-automation'], 
+        ignore_default_args=['--enable-automation', '--no-sandbox'], 
         args=[
-            '--start-maximized',
-            '--disable-blink-features=AutomationControlled',
-            '--disable-infobars'
+            '--start-maximized'
         ]
     )
 
     page = browser.pages[0] if browser.pages else browser.new_page()
-
-    # js
-    page.add_init_script('''
-        // webdriver
-        Object.defineProperty(navigator, 'webdriver', { get: () => undefined });
-
-        // plugins
-        Object.defineProperty(navigator, 'plugins', { get: () => [1, 2, 3, 4, 5] });
-
-        // languages
-        Object.defineProperty(navigator, 'languages', { get: () => ['en-US', 'en'] });
-
-        // permissions
-        const originalQuery = window.navigator.permissions.query;
-        window.navigator.permissions.query = (parameters) =>
-            parameters.name === 'notifications'
-                ? Promise.resolve({ state: Notification.permission })
-                : originalQuery(parameters);
-    ''')
+    stealth_sync(page)
 
     # page.goto('https://music.apple.com/in/home')
     page.goto('https://whatismyviewport.com/')
